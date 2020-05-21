7e9157ddbc17e12c78f916bc59f819540355f5cf
yami-er <queenyani@gmail.com>
2020-05-21 01:44:12 -0400

    video subclss extending from library items

---
+++ Video.java
@@ -0,0 +1,37 @@
+public class Video extends LibraryItem{
+    //add more variables for video
+    boolean highDefinition;
+    boolean rated;
+    boolean cartoon;
+    public Video(String title, int numberOfCopy,boolean highDefinition, boolean rated, boolean cartoon){
+        super(title, numberOfCopy);
+        this.highDefinition= highDefinition;
+        this.rated = rated;
+        this.cartoon = cartoon;
+    }
+    //getter and setter
+    public boolean getHighDefinition(){
+        return highDefinition;
+    }
+    public boolean getRated(){
+        return rated;
+    }
+    public boolean getCartoon(){
+        return cartoon;
+    }
+    public void setHighDefinition(boolean highDefinition){
+        this.highDefinition= highDefinition;
+    }
+
+    public void setRated(boolean rated){
+        this.rated= rated;
+    } 
+    public void setCartoon(boolean cartoon){
+        this.cartoon= cartoon;
+    }  
+    //to override the loan day method from library item 
+    @Override
+    public int loanDays(){
+      return super.loanDays() + 3;
+    }
+}
