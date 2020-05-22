d498ecb01dae0b63127bdf9b7cd451ec59c60ec0
yami-er <queenyani@gmail.com>
2020-05-22 00:51:33 -0400

    classes used to create relationship

---
+++ Author.java
@@ -0,0 +1,9 @@
+public class Author {
+    String firstName;
+    String lastName;
+    Author(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName; 
+ 
+    }
+}
---
+++ PublishingCompany.java
@@ -0,0 +1,9 @@
+public class PublishingCompany {
+    String publishingName;
+    String publishingAddress;
+
+    PublishingCompany(String publishingName, String publishingAddress){
+      this.publishingName = publishingName;
+      this.publishingAddress = publishingAddress;
+    }
+}
