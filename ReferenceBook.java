db5024c29c5f7574eb3b66ba59ffc75d87998bc8
yami-er <queenyani@gmail.com>
2020-05-22 01:22:48 -0400

    modified commit on book and its child class

--- ReferenceBook.java
+++ ReferenceBook.java
@@ -1,9 +1,10 @@
+import java.util.*;
 public class ReferenceBook extends Book {
     //set new variable local
     String subject;
     public ReferenceBook(String title, String authorName, String subject, int yearPublished, int numberOfCopy,
-    int edition, boolean used, boolean hardCover){
-        super(title, authorName, yearPublished, numberOfCopy, edition, used, hardCover);
+    int edition, boolean used, boolean hardCover,List<Author> authors, List<PublishingCompany> publishingCompany){
+        super(title, authorName, yearPublished, numberOfCopy, edition, used, hardCover,authors,publishingCompany);
         this.subject = subject;
     }
     //getter & setter
